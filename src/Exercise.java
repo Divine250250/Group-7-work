09e7b1b42c0e8978f285c9814976726c2bb38c72
Divine250250 <ingabireuwasedivine@gmail.com>
2025-09-25 12:19:47 +0000

    final

--- src/Exercise.java
+++ src/Exercise.java
@@ -63,5 +63,22 @@ public class Exercise {
 
         System.out.println("The area of the pentagon is "+ roundedArea);
 
+        // Question 3
+
+        System.out.println("Enter a letter: ");
+        String letter = input.nextLine();
+
+        switch (letter){
+            case "a","b","c" -> System.out.println("The number is 2");
+            case "d","e","f" -> System.out.println("The number is 3");
+            case "g","h","i" -> System.out.println("The number is 4");
+            case "j","k","l" -> System.out.println("The number is 5");
+            case "m","n","o" -> System.out.println("The number is 6");
+            case "p","q","r","s"-> System.out.println("The number is 7");
+            case "t","u","v" -> System.out.println("The number is 8");
+            case "W","x","y","z"-> System.out.println("The number is 9");
+            default -> System.out.println("Wrong input.");
+        }
+
     }
 }
